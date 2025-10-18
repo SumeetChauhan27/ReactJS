d974597401e0aabf80237962e1c3960a0ba33bbf
SumeetChauhan27 <sumeetchuhan428@gmail.com>
2025-10-18 23:09:08 +0530

    Components

---
+++ 02-Compnents/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Card from './Components/card'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello and Welcome..</h1>
+      <Card />
+    </div>
+  )
+}
+
+export default App 
---
+++ 02-Compnents/src/Components/card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+       <div className='card'>
+        <h1>Sumeet Chauhan </h1>
+        <p>  my name is Sumeet chauhan i am the beginer for reactJS
+        </p>
+    </div>
+    </div>
+  )
+}
+
+export default card
